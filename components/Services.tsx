c983afa2337a35fc1a45f591e350ef996d103ad1
Andrew Lonati <andrew.roman117@gmail.com>
2025-09-10 19:32:12 -0400

    Update Services.tsx

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,4 +1,3 @@
-// components/Services.tsx
 import FractionalGtmCard from './services/FractionalGtmCard';
 import SystemsAutomationCard from './services/SystemsAutomationCard';
 
@@ -11,7 +10,7 @@ export default function Services() {
           Two ways to engage, endless ways to deliver. Simple hourly pricing + tailored scopes to fit your growth stage.
         </p>
 
-        <div className="mt-8 grid md:grid-cols-2 gap-6">
+        <div className="mt-8 flex flex-col gap-8">
           <FractionalGtmCard />
           <SystemsAutomationCard />
         </div>
