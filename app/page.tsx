61dfdf66fbf127920da78a700ead9e784de036f6
Andrew Lonati <andrew.roman117@gmail.com>
2025-09-10 20:20:20 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 // app/page.tsx
 import Hero from '@/components/Hero';
 import Services from '@/components/Services';
-import SolutionsResultsLite from '@/components/SolutionsResultsLite';
 import Testimonials from '@/components/Testimonials';
 import AboutImpactCard from '@/components/AboutImpactCard';
 import Contact from '@/components/Contact';
@@ -10,25 +9,11 @@ import Footer from '@/components/Footer';
 export default function Page() {
   return (
     <>
-      {/* 1) Hero */}
       <Hero />
-
-      {/* 2) Services / How we help */}
       <Services />
-
-      {/* 3) Results (Solutions & Results) */}
-      <SolutionsResultsLite />
-
-      {/* 4) Testimonials */}
       <Testimonials />
-
-      {/* 5) About & Track Record (flip card as-is) */}
       <AboutImpactCard />
-
-      {/* 6) Contact */}
       <Contact />
-
-      {/* 7) Footer */}
       <Footer />
     </>
   );
